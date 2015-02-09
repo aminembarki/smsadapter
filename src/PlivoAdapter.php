185f8ec41a51b27ae1706625e4faa4f539d89411
Amine MBARKI <a.mbarki@dm73.net>
2015-02-09 17:07:55 +0000

    init adapters

---
+++ src/PlivoAdapter.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: amine
+ * Date: 2/9/2015
+ * Time: 4:25 PM
+ */
+require '../vendor/autoload.php';
+
+class PlivoAdapter implements SmsAdapterInterface{
+
+    protected $service;
+
+    public function __construct()
+    {
+
+    }
+
+    public function authenticate(array $options)
+    {
+        $this->service =  new Services_Twilio($options['api_key'], $options['api_secret']);
+
+    }
+
+    public function sendMessage( $to, $from, $message, $unicode=null )
+    {
+        $response = $this->service->account->messages->create(array(
+            "From" => $from,
+            "To" => $to,
+            "Body" => $message,
+        ));
+
+        return $response->sid;
+    }
+} 
